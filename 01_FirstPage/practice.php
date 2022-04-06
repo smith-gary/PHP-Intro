5e75953d7ac0e900f3b350b9cb431796496b6d5c
gary_s <smithgd34@gmail.com>
2022-04-06 15:51:32 -0500

    completed first practice

--- 01_FirstPage/practice.php
+++ 01_FirstPage/practice.php
@@ -4,6 +4,6 @@
 		<title>My First PHP Web Page</title>
 	</head>
 	<body>
-		<?php print('Hello friends!'); ?>
+		<?php print("PHP print puts it on the page like normal HTML"); ?>
 	</body>
 </html>
