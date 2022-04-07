a7b9b1937271bd740099dd96e6b4b5a65800627b
gary_s <smithgd34@gmail.com>
2022-04-07 11:40:26 -0500

    associative arrays

--- 07_AssociativeArrays/practice.php
+++ 07_AssociativeArrays/practice.php
@@ -1,19 +1,26 @@
 <?php
 	
 	// Constants
-	
+	define("TITLE", "Associative Arrays");
 	
 	// Custom Variables
+	$date = date('Y');
+	$lesson = 7;
+	$name = "Gary";
 
 	
 	// Moustache Associative Array
-
+	$moustache = array(
+		"name" => "Handlebar",
+		"creep_factor" => "High",
+		"growth" => 14
+	);
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHP <!-- PAGE TITLE --></title>
+		<title>PHP <?php ?></title>
 		<link href="/assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
@@ -22,15 +29,15 @@
 				<img src="/assets/img/logo.png" alt="PHP">
 			</a>
 			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- PAGE TITLE --></small></h1>
+			<h1>Tutorial <?php echo $lesson ?>: <small><?php echo TITLE ?></small></h1>
 			<hr>
 			
 			<h2>Your Example</h2>
 			
 			<div class="sandbox">
 			
-				<h2>The <!-- MOUSTACHE NAME --> Moustache!</h2>
-				<p>This moustache is quite the dirt squirrel! It boasts a creep factor of <strong><!-- CREEP FACTOR --></strong> and takes <strong><!-- GROWTH DAYS --> days</strong> to grow on average.</strong></p>
+				<h2>The <?php echo $moustache[name] ?> Moustache!</h2>
+				<p>This moustache is quite the dirt squirrel! It boasts a creep factor of <strong><?php echo $moustache[creep_factor] ?></strong> and takes <strong><?php echo $moustache[growth] ?> days</strong> to grow on average.</strong></p>
 				
 			</div><!-- end sandbox -->
 			
@@ -38,7 +45,7 @@
 			
 			<hr>
 			
-			<small>&copy;<!-- YEAR --> - <!-- YOUR NAME --></small>
+			<small>&copy;<?php echo $date ?> - <?php echo $name ?></small>
 		</div><!-- end wrapper -->
 		
 		<div class="copyright-info">
